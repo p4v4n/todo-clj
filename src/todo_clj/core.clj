b070852a097665606df339b2dac2024cf2281389
p4v4n <pavan.barcelona@gmail.com>
2017-12-17 23:08:15 +0530

    Add ring server+jetty with a hello fn

--- src/todo_clj/core.clj
+++ src/todo_clj/core.clj
@@ -1,1 +1,11 @@
-(ns todo-clj.core)
+(ns todo-clj.core
+  (:require [ring.adapter.jetty :as jetty]))
+
+(defn hello [req]
+  {:status 200
+   :body "Hello!"
+   :headers {}})
+
+(defn -main [port]
+  (jetty/run-jetty hello
+                   {:port (Integer. port)}))
