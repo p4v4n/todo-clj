e02c785fa50ea742a0642c02b05c5e3b13322a96
p4v4n <pavan.barcelona@gmail.com>
2017-12-18 10:31:09 +0530

    Add routes using compojure

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.6.3"]]
+                 [ring "1.6.3"]
+                 [compojure "1.6.0"]]
   :main todo-clj.core
   :profiles {:dev
              {:main todo-clj.core/-dev-main}})
--- src/todo_clj/core.clj
+++ src/todo_clj/core.clj
@@ -1,16 +1,22 @@
 (ns todo-clj.core
   (:require [ring.adapter.jetty :as jetty]
-            [ring.middleware.reload :refer [wrap-reload]]))
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]))
 
 (defn hello [req]
   {:status 200
    :body "Hello!!"
    :headers {}})
 
+(defroutes routes
+  (GET "/" [] hello)
+  (not-found "Page not found."))
+
 (defn -main [port]
-  (jetty/run-jetty hello
+  (jetty/run-jetty routes
                    {:port (Integer. port)}))
 
 (defn -dev-main [port]
-  (jetty/run-jetty (wrap-reload #'hello)
+  (jetty/run-jetty (wrap-reload #'routes)
                    {:port (Integer. port)}))
