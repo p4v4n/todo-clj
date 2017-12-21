a09e68342990f6b9bacba54f11efd95a34aec306
p4v4n <pavan.barcelona@gmail.com>
2017-12-21 15:14:29 +0530

    Add a form to /items page   -Add handle-create-item to create a item and redirect to /items.

--- src/todo_clj/items/handler.clj
+++ src/todo_clj/items/handler.clj
@@ -10,4 +10,16 @@
      :headers {}
      :body (str "<html><head></head><body><div>"
                 (item-names  items)
+                "</div><form method=\"POST\" action=\"/items\">"
+                "<input type=\"text\" name=\"name\" placeholder=\"name\">"
+                "<input type=\"text\" name=\"description\" placeholder=\"description\">"
+                "<input type=\"submit\">"
                 "</div></body></html>")}))
+
+(defn handle-create-item [req]
+  (let [name (get-in req [:params "name"])
+        description (get-in req [:params "description"])
+        item-id (t/create-item name description)]
+    {:status 302
+     :headers {"Location" "/items"}
+     :body ""}))
