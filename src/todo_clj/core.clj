7a84539c20e8c88a577be7f7a68bec3207b00c51
p4v4n <pavan.barcelona@gmail.com>
2017-12-21 11:31:34 +0530

    Add wrap-params as middleware to parse query strings

--- src/todo_clj/core.clj
+++ src/todo_clj/core.clj
@@ -1,7 +1,9 @@
 (ns todo-clj.core
+  (:require [todo-clj.items.modelt :as items])
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
-            [compojure.core :refer [defroutes GET]]
+            [ring.middleware.params :refer [wrap-params]]
+            [compojure.core :refer [defroutes ANY GET POST PUT DELETE]]
             [compojure.route :refer [not-found]]
             [ring.handler.dump :refer [handle-dump]])
   (:gen-class))
@@ -16,10 +18,13 @@
   (GET "/request" [] handle-dump)
   (not-found "Page not found."))
 
+(def app
+  (wrap-params  routes))
+
 (defn -main [port]
-  (jetty/run-jetty routes
+  (jetty/run-jetty app
                    {:port (Integer. port)}))
 
 (defn -dev-main [port]
-  (jetty/run-jetty (wrap-reload #'routes)
+  (jetty/run-jetty (wrap-reload #'app)
                    {:port (Integer. port)}))
