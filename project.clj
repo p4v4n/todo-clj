2a947c965985abc0c158e62993b5ea5533a99587
p4v4n <pavan.barcelona@gmail.com>
2017-12-21 17:51:03 +0530

    Update project dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.6.3"]
-                 [compojure "1.6.0"]]
+                 [compojure "1.6.0"]
+                 [hiccup "1.0.5"]
+                 [org.clojure/java.jdbc "0.7.4"]
+                 [com.h2database/h2 "1.4.196"]]
   :main todo-clj.core
   :profiles {:dev
              {:main todo-clj.core/-dev-main}})
