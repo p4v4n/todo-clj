3f244e96873a3744f6d71e0f6066881a4683d3e4
p4v4n <pavan.barcelona@gmail.com>
2017-12-22 11:58:13 +0530

    Add fn to generate create-item form element

--- src/todo_clj/items/view.clj
+++ src/todo_clj/items/view.clj
@@ -2,13 +2,37 @@
   (:require [hiccup.page :refer [html5]]
             [hiccup.core :refer [html h]]))
 
+(defn new-item []
+  (html5
+    [:form.form-horizontal
+     {:method "POST" :action "/items"}
+     [:div.form-group
+      [:label.control-label.col-sm-2 {:for :name-input}
+       "Name"]
+      [:div.col-sm-10
+       [:input#name-input.form-control
+        {:name :name
+         :placeholder "Name"}]]]
+     [:div.form-group
+      [:label.control-label.col-sm-2 {:for :desc-input}
+       "Description"]
+      [:div.col-sm-10
+       [:input#desc-input.form-control
+        {:name :description
+         :placeholder "Description"}]]]
+     [:div.form-group
+      [:div.col-sm-offset-2.col-sm-10
+       [:input.btn.btn-primary
+        {:type :submit
+         :value "Create Item"}]]]]))
+
 (defn items-page [items]
   (html5 {:lang :en}
          [:head
           [:title "todo-app"]
           [:meta {:name :viewport
                   :content "width=device-width, initial-scale=1.0"}]
-          [:link {:href "/bootstrap/css/bootstrap.min.css"
+          [:link {:href "/css/bootstrap.min.css"
                   :rel :stylesheet}]]
          [:body
           [:div.container
@@ -25,6 +49,9 @@
                   [:tr
                    [:td (h (:name i))]
                    [:td (h (:description i))]])]]
-              [:div.col-sm-offset-1 "There are no items."])]]
+              [:div.col-sm-offset-1 "There are no items."])]
+           [:div.col-sm-6
+            [:h2 "Create a New Item"]
+            (new-item)]]
           [:script {:src "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min/js"}]
-          [:script {:src "/bootstrap/js/bootstrap.min.js"}]]))
+          [:script {:src "/js/bootstrap.min.js"}]]))
