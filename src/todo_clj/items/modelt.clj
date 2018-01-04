59924e2f9c7816e238c888b8f48e5e232b21e376
p4v4n <pavan.barcelona@gmail.com>
2018-01-04 11:21:54 +0530

    Add user-store to store user-pass info   -Add create-user,get-user,get-userid-from-username fns

--- src/todo_clj/items/modelt.clj
+++ src/todo_clj/items/modelt.clj
@@ -1,6 +1,9 @@
-(ns todo-clj.items.modelt)
+(ns todo-clj.items.modelt
+  (:require [buddy.hashers :as hashers]))
 
 (def t-db (atom {}))
+
+(def user-db (atom {}))
 
                                         ;----------------------
 
@@ -16,7 +19,30 @@
 (defn date-now []
   (.format (java.text.SimpleDateFormat. "MM/dd/yyyy") (new java.util.Date)))
 
-;;-------------------
+
+;;------------------Users db
+
+(defn create-user [user]
+  (let [password (:password user)
+        user-id (generate-uuid)]
+    (swap! user-db assoc user-id (-> user
+                                     (dissoc :password)
+                                     (assoc :id user-id
+                                            :password-hash (hashers/encrypt password))))))
+
+(defn get-user [user-id]
+  (get @user-db user-id))
+
+;;gets user-id given user-name and pass
+(defn get-user-by-username-and-password [username password]
+  (prn username password)
+  (->> (vals @user-db)
+       (filter #(and (= username (:username %))
+                     (hashers/check password (:password-hash %))))
+       first
+       :id))
+
+;;-------------------Items db
 
 (defn create-item [name description]
   (if (and (name-valid? name) (description-valid? description))
