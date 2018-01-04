c276f76bce9cd7a18eaabe0a2dfce5a7b881ec76
p4v4n <pavan.barcelona@gmail.com>
2018-01-04 12:03:52 +0530

    Add buddy to project dependencies

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [compojure "1.6.0"]
                  [hiccup "1.0.5"]
                  [org.clojure/java.jdbc "0.7.4"]
-                 [com.h2database/h2 "1.4.196"]]
+                 [com.h2database/h2 "1.4.196"]
+                 [buddy "2.0.0"]]
   :main todo-clj.core
   :profiles {:dev
              {:main todo-clj.core/-dev-main}})
